0076dfca02a130de7817a538f1af58b27e97a501
Jack feng <48426950+fxianglong@users.noreply.github.com>
2019-11-18 23:57:31 +0800

    11.18练习
    11.18练习

--- test.cpp
+++ test.cpp
@@ -1,90 +1,54 @@
 #include<iostream>
+#include<string>
 using namespace std;
-////输入两个整数a,b用自定义函数max()求他们的最大值
-//int max(int a, int b)
+//class Person
 //{
-//	int c;
-//	if (a > b)
-//		c = a;
-//	else c = b;
-//	return c;
-//}
-//void main()
+//public:
+//	string _name; // 姓名
+//};
+//class Student : public Person
 //{
-//	int a, b;
-//	cin >> a >> b;
-//	cout << "最大值为：" << max(a, b) << endl;
-//}
-////显示用户输入的绝对值
-//void main()
+//protected:
+//	int _num; //学号
+//};
+//class Teacher 
 //{
-//	int x, y;
-//	cin >> x;
-//	if (x < 0)
-//		y = -x;
-//	else y = x;
-//	cout << y << endl;
-//}
-////编写一个程序，将用户输入的整数反向显示出来
-//void main()
+//protected:
+//	int _id; // 职工编号
+//};
+//class Assistant : public Student, public Teacher
 //{
-//	int x=0,y=0;
-//	cin >> x;
-//	while (x > 0)
-//	{
-//		y = y * 10 + (x % 10);
-//		x /= 10;
-//	}
-//	cout << y <<endl;
-//	system("pause");
-//}
-////do……while编程，将用户输入的整数反向显示出来
-//void main()
+//protected:
+//	string _majorCourse; // 主修课程
+//};
+
+//int main()
 //{
-//	int x = 0, y = 0;
-//	cin >> x;
-//	do{
-//		y = y * 10 + (x % 10);
-//				x /= 10;
-//	} while (x > 0);
-//	cout << y << endl;
-//	system("pause");
+//	Assistant a;
+//	/*a.Person::_name = "peter";*/
 //
-//}
-////for编程，将用户输入的整数反向显示出来
-//void main()
-//{
-//	int x = 0, y = 0;
-//	cin >> x;
-//	for (; x > 0; y = y * 10 + (x % 10), x /= 10);
-//	cout << y << endl;
+//	// 需要显示指定访问哪个父类的成员可以解决二义性问题，但是数据冗余问题无法解决
+//	a.Student::_name = "xxx";
+//	a.Teacher::_name = "yyy";
+//	a.Teacher::_name = "swq";
 //	system("pause");
-//
+//	return 0;
 //}
-////从键盘读入用户输入的数，对其求和，当用户输入数0时，便是输入结束
-//void main()
-//{
-//	int sum = 0, x = 0;
-//	while (1)
-//	{
-//		cin >> x;
-//		if (x == 0)
-//			break;
-//		sum += x;
-//	}
-//	cout << sum << endl;
-//	system("pause");
-//}
-////编写一个程序，求满足条件1^2+2^2+3^2+……+n^2<=1000的最大的n
-//void main()
-//{
-//	int n = 0, sum = 0;
-//	while (1)
-//	{
-//		sum += n * n;
-//		if (sum > 1000) goto end;
-//		n++;
-//	}
-//end:cout << "最大的n为：" << n << endl;
-//	system("pause");
-//}
+
+class Person {
+public:
+	virtual ~Person() { cout << "~Person()" << endl; }
+};
+class Student : public Person {
+public:
+		virtual ~Student() { cout << "~Student()" << endl; }
+};
+int main()
+{
+	Person* p1 = new Person;
+	Person* p2 = new Student;
+	delete p1;
+	delete p2;
+	system("pause");
+	return 0;
+}
